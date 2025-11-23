be846378680ca35315a429790113dceeb1040d50
polar <polar@dona.one>
2025-11-23 17:39:45 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { AuthSessionProvider } from './supabase/AuthSessionProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthSessionProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthSessionProvider>
   </React.StrictMode>,
 );
